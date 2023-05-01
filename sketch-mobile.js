2812bf69f7f56d020dab9faace6262389723304b
elliotmcdonald <37638968+elliotmcdonald@users.noreply.github.com>
2023-05-01 11:29:17 +0200

    Update sketch-mobile.js

--- sketch-mobile.js
+++ sketch-mobile.js
@@ -6,7 +6,7 @@ function setup() {
   myCanvas.parent('canvas-intro-container');
   noStroke();
   fill(255);
-  circleSize = min(windowWidth, windowHeight) * 0.9;
+  circleSize = min(windowWidth, windowHeight) * 1.9;
 }
 
 function draw() {
